7dc9a3477e54bdd471edadba8573ab7477ad31bb
Ryder Khoi Daniel <ryderdaniel83@gmail.com>
2021-01-01 23:21:17 +0800

    add powerline configs

---
+++ powerline-shell/themes/my_theme.py
@@ -0,0 +1,31 @@
+from powerline_shell.themes.default import DefaultColor
+
+class Color(DefaultColor):
+    USERNAME_ROOT_BG = 88
+    USERNAME_BG = 23
+    USERNAME_FG = 187
+
+    HOSTNAME_BG = 214
+    HOSTNAME_FG = 234
+
+    HOME_SPECIAL_DISPLAY = True
+    HOME_BG = 202
+    HOME_FG = 234
+    PATH_BG = 23
+    PATH_FG = 187
+    CWD_FG = 187
+    SEPARATOR_FG = 202
+
+    READONLY_BG = 41
+    READONLY_FG = 234
+
+    SSH_BG = 77
+    SSH_FG = 234
+
+    CMD_PASSED_FG = 187
+    CMD_PASSED_BG = 240
+    CMD_FAILED_FG = 187
+    CMD_FAILED_BG = 160
+
+    TIME_FG = 234
+    TIME_BG = 187
